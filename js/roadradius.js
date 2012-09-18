ff5ce9cffec6ce35135c10190836a596d32632a6
rjshade <robbie.shade@gmail.com>
2012-09-18 13:55:35 +0100

    Tidying up

--- js/roadradius.js
+++ js/roadradius.js
@@ -4,51 +4,46 @@ var directionsService;
 var gRoutes    = new Array(); // store google.maps.DirectionRoute objects
 var gPolylines = new Array(); // store polyline overlays
 var gMarkers   = new Array(); // store marker overlays
-var searchRadius = 50; // in kilometers...
+var searchRadius = 30; // in kilometers...
 
 // Try to geolocate to center initial map... if geolocation fails
 // then we default to centering on Oxford, England
 function initialize()
 {
-    var mapOptions = {
-            zoom: 9, //required
-            center: oxford, //required
-            mapTypeId: google.maps.MapTypeId.ROADMAP //required
-    };
+  var mapOptions = {
+    zoom: 9, //required
+    center: oxford, //required
+    mapTypeId: google.maps.MapTypeId.ROADMAP //required
+  };
 
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    directionService = new google.maps.DirectionsService();
-    directionsRenderer = new google.maps.DirectionsRenderer({ map: map }); 
+  map = new google.maps.Map(document.getElementById("gmaps-canvas"), mapOptions);
+  directionService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer({ map: map }); 
 
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(gotGeolocation,noGeolocation,{'enableHighAccuracy':true,'timeout':5000,'maximumAge':0});
+  } else {
+    map.setCenter(oxford);
+  }
 
-    if(navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(gotGeolocation,noGeolocation,{'enableHighAccuracy':true,'timeout':5000,'maximumAge':0});
-    }
-    else
-    {
-        map.setCenter(oxford);
-    }
+  function gotGeolocation(pos) {
+    var myLocation = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+    map.setCenter(myLocation);
+  }
 
-    function gotGeolocation(pos)
-    {
-        var myLocation = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-        map.setCenter(myLocation);
-    }
-    function noGeolocation(err)
-    {
-        map.setCenter(oxford);
-    }
+  function noGeolocation(err) {
+    map.setCenter(oxford);
+  }
 
-    google.maps.event.addListener(map, 'click', function(event) 
-                                                {startHere(event.latLng); });
+  google.maps.event.addListener(map, 'click', function(event) {
+    startHere(event.latLng);
+  });
 }
 
 
 // makes the call to the google maps direction service to find
 // a route from origin to a destination distance meters away on given heading
-function addNewRoute( origin, distance, heading )
-{
+function addNewRoute( origin, distance, heading ) {
   var destination = google.maps.geometry.spherical.computeOffset( origin, distance, heading )
 
   var request = {
@@ -56,98 +51,88 @@ function addNewRoute( origin, distance, heading )
     destination: destination,
     travelMode: google.maps.DirectionsTravelMode.DRIVING
   }
-  
+
   //alert("making request..." + request.origin + "   " + request.destination);
-  directionService.route(request, 
-                         function(result,status)
-                         {
-                             if (status == google.maps.DirectionsStatus.OK)
-                             {
-                                var route = pruneRoute( result.routes[0], distance );
-                                //var route = result.routes[0];
-                                gRoutes.push(route);
+  directionService.route(request, function(result,status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      var route = pruneRoute( result.routes[0], distance );
+      //var route = result.routes[0];
+      gRoutes.push(route);
 
-                                var routePolyline = 
-                                               new google.maps.Polyline({
-                                               path: route.overview_path,
-                                               strokeColor: "#FF0000",
-                                               strokeOpacity: 1.0,
-                                               strokeWeight: 2,
-                                               map: map });
+      var routePolyline = 
+      new google.maps.Polyline({
+        path: route.overview_path,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+        map: map
+      });
 
-                                gPolylines.push( routePolyline );
+      gPolylines.push( routePolyline );
 
-                                
-                                // add markers at the ends of all routes
-                                var endLocation = route.overview_path[route.overview_path.length - 1]
-                                marker = new google.maps.Marker({
-                                         position: endLocation,
-                                         map: map
-                                });
-                                // markers can be clicked to spawn new route finding
-                                google.maps.event.addListener(marker, 'click', function(){ startHere( endLocation ) } )
-                                gMarkers.push(marker);
-                             } 
-                             else 
-                             {
-                                 //alert("Directions query failed: " + status);
-                             }
-                         });
+      // add markers at the ends of all routes
+      var endLocation = route.overview_path[route.overview_path.length - 1]
+      marker = new google.maps.Marker({
+        position: endLocation,
+        map: map
+      });
+      // markers can be clicked to spawn new route finding
+      google.maps.event.addListener(marker, 'click', function(){ startHere( endLocation ) } )
+        gMarkers.push(marker);
+    } else {
+      //alert("Directions query failed: " + status);
+    }
+  });
 }
 
 // clears all the overlays and resets the global arrays
-function clearAllRoutes()
-{
-    gPolylines.forEach( function( overlay, index, array ){ overlay.setMap(null); } );
-    gMarkers.forEach( function( overlay, index, array ){ overlay.setMap(null); } );
+function clearAllRoutes() {
+  gPolylines.forEach( function( overlay, index, array ){ overlay.setMap(null); } );
+  gMarkers.forEach( function( overlay, index, array ){ overlay.setMap(null); } );
 
-    gPolylines.length = 0;
-    gRoutes.length = 0;
-    gMarkers.length = 0;
+  gPolylines.length = 0;
+  gRoutes.length = 0;
+  gMarkers.length = 0;
 }
 
 // finds routes from 'origin' to points on a circle of radius 'distance'
 // every 'interval' degrees
-function findRadiusRoutes( origin, distance, interval )
-{
-    for( var i = 0; i <= 360; i += interval )
-    { addNewRoute( origin, distance, i ); }
+function findRadiusRoutes( origin, distance, interval ) {
+  for( var i = 0; i <= 360; i += interval )
+  { addNewRoute( origin, distance, i ); }
 }
 
 // given a google.maps.DirectionsRoute object it chops waypoints off the
 // end of it to make its length <= 'length'
-function pruneRoute( route, length )
-{
-    var distance = 0;
-    var waypoint = 1;
-    while( distance < length && waypoint < route.overview_path.length )
-    {
-        distance += 
-            google.maps.geometry.spherical.computeDistanceBetween( 
-                                    route.overview_path[waypoint - 1],
-                                    route.overview_path[waypoint] );
+function pruneRoute( route, length ) {
+  var distance = 0;
+  var waypoint = 1;
+  while( distance < length && waypoint < route.overview_path.length ) {
+    distance += 
+      google.maps.geometry.spherical.computeDistanceBetween( 
+          route.overview_path[waypoint - 1],
+          route.overview_path[waypoint] );
 
-        waypoint += 1;
-        //alert ("distance = " + distance + "  waypoint = " + waypoint);
-    }
-    
-    //TODO this doesn't recalculate latlng bounds
-    var pruned_route = route;
-    pruned_route.legs          = pruned_route.legs.slice(0, waypoint);
-    pruned_route.overview_path = pruned_route.overview_path.slice(0, waypoint);
-    return pruned_route;
+    waypoint += 1;
+    //alert ("distance = " + distance + "  waypoint = " + waypoint);
+  }
+
+  //TODO this doesn't recalculate latlng bounds
+  var pruned_route = route;
+  pruned_route.legs          = pruned_route.legs.slice(0, waypoint);
+  pruned_route.overview_path = pruned_route.overview_path.slice(0, waypoint);
+  return pruned_route;
 }
 
-function startHere(location)
-{
-    marker = new google.maps.Marker({
-             position: location,
-             map: map
-    });
-    gMarkers.push(marker);
+function startHere(location) {
+  marker = new google.maps.Marker({
+    position: location,
+         map: map
+  });
+  gMarkers.push(marker);
 
-    searchRadius = parseInt( $('#distance_input').val() );
-    findRadiusRoutes( location, searchRadius * 1000, 10);
+  searchRadius = parseInt( $('#distance_input').val() );
+  findRadiusRoutes( location, searchRadius * 1000, 45);
 }
 
 // gets everything going once the page has loaded...
